f82649ad05d934fd698cb10d3e6d06a61c5336a1
Anton Simeonov <aplamenov@gmail.com>
2024-06-10 14:50:39 +0300

    Refactoring of Index
    Refactoring of Index

--- Movie-Magic-Workshop/index.js
+++ Movie-Magic-Workshop/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const hbsConfig = require('./hbsConfig');
+const expressConfig = require('./expressConfig')
 
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 
-app.use((req, res) => {
-    res.status(404).redirect('/404');
-  });
+hbsConfig(app)
+
+expressConfig(app);
+
+
 
 app.listen(port, console.log(`The app is running ${port}`));
 
