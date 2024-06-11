192d15841d960b8e7d5887b8933c155fb2886a56
Anton Simeonov <aplamenov@gmail.com>
2024-06-11 16:03:22 +0300

    search page and search functionality

--- Movie-Magic-Workshop/services/movieService.js
+++ Movie-Magic-Workshop/services/movieService.js
@@ -16,13 +16,31 @@ exports.getAll = () => {
   // return Array.form(movies);
 };
 
+exports.search = (title, genre, year) => {
+  let result = movies.slice();
+
+  if (title){
+    result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+  }
+
+  if (genre) {
+    result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
+  }
+
+  if (year) {
+    result = result.filter(movie => movie.year === year);
+  }
+  
+  return result;
+};
+
 exports.getOne = (movieId) => {
-  const movie = movies.find(movie => movie._id == movieId)
-  return movie
+  const movie = movies.find(movie => movie._id == movieId);
+  return movie;
 };
 
 
 exports.create = (movieData) => {
     movieData._id = movies[movies.length - 1]._id + 1;
-    movies.push(movieData)
+    movies.push(movieData);
 }; 
