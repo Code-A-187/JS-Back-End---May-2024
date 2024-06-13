ec3ccb60c918c8c3831e9dff766995347c6fef27
Anton Simeonov <aplamenov@gmail.com>
2024-06-13 13:22:59 +0300

    Refactoring home and details to use MongoDB

--- Movie-Magic-Workshop/services/movieService.js
+++ Movie-Magic-Workshop/services/movieService.js
@@ -1,25 +1,14 @@
 const Movie = require('../models/Movie');
 
-const movies = [{
-  _id: 1,
-  title: 'Jungle Cruise' ,
-  genre: 'Adventure',
-  director: 'Jaume Collet-Serra',
-  year: '2021',
-  imageUrl: '/img/jungle-cruise.jpeg',
-  rating: '5',
-  description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.'
-}];
-
-
 exports.getAll = () => Movie.find();
 
   // return [...movies];
   // return Array.form(movies);
 
 
-exports.search = (title, genre, year) => {
-  let result = movies.slice();
+  // TODO: filter result in mongoDB
+exports.search = async (title, genre, year) => {
+  let result = await Movie.find().lean();
 
   if (title){
     result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
@@ -36,9 +25,7 @@ exports.search = (title, genre, year) => {
   return result;
 };
 
-exports.getOne = (movieId) => {
-  const movie = movies.find(movie => movie._id == movieId);
-  return movie;
-};
+exports.getOne = (movieId) => Movie.findById(movieId).lean();
+ 
 
 exports.create =(movieData) => Movie.create(movieData);
