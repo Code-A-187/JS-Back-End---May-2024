4a51276725fccec127d59a5611ced359fe8cdce3
Anton Simeonov <aplamenov@gmail.com>
2024-06-13 13:40:58 +0300

    connect cast page

--- Movie-Magic-Workshop/routes.js
+++ Movie-Magic-Workshop/routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const path = require('path');
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
+const castController = require('./controllers/castController')
 
 
 router.use(homeController);
 router.use(movieController);
+router.use('/cast', castController)
 
 router.get('*', (req, res) => {
   res.redirect('/404')
