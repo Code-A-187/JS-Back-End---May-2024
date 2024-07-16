08df6c3e7c4745e2c206d1e2e4eb500d8e222e7e
Anton Simeonov <aplamenov@gmail.com>
2024-07-16 19:33:54 +0300

    mock database

---
+++ 07.Workshop-REST-demo/src/database.js
@@ -0,0 +1,19 @@
+const database = {
+    1: {
+        _id: 1,
+        name: 'Peter',
+        age: 27
+    },
+    2: {
+        _id: 2,
+        name: 'John',
+        age: 33
+    },
+    3: {
+        _id: 3,
+        name: 'Marry',
+        age: 19
+    },
+};
+
+module.exports = { database };
