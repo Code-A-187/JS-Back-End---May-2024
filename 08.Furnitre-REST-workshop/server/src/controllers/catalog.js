e133db291ed46ef12099610f20e7cb681b661ea4
Anton Simeonov <aplamenov@gmail.com>
2024-07-17 15:45:43 +0300

    config routes and connection with client

---
+++ 08.Furnitre-REST-workshop/server/src/controllers/catalog.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const catalogRouter  = Router();
+
+catalogRouter.get('/', async (req, res) => {
+    res.json([]);
+});
+
+catalogRouter.get('/:id', async (req, res) => {
+    res.render({});
+});
+
+module.exports = { catalogRouter };
