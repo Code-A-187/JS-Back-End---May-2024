98aa71a9b74eb4582eb40709a4e6776fdd1cff12
Anton Simeonov <aplamenov@gmail.com>
2024-07-22 13:45:04 +0300

    configure server and client connection by CORS middleware

--- 06.Workshop -REST-Api-Papazov/server/index.js
+++ 06.Workshop -REST-Api-Papazov/server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello world'
+        message: 'Hello World!'
     });
 });
 
-app.listen(5000, () => console.log('Server is listening on port 5000...'))
+app.get('/data/catalog', (req, res) => {
+    res.json([]);
+});
+
+app.listen(3030, () => console.log('Server is listening on port 3030...'))
